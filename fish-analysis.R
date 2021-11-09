eb55749fc95d7210de7420e13d339397107821b7
mcwrazej <mcwrazej@mail.ubc.ca>
2021-11-09 15:33:53 -0800

    Added description of what code is doing

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,6 +1,8 @@
 library(dplyr)
+#Creating new categorical size column
 fish_data_cat = fish_data %>% 
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+#Creating category cut-off size 
 fish_data_cat = fish_data %>% 
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
